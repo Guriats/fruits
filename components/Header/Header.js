617c19ebcc79204307cb1771b08191ec3160b1a1
david crespo <davidcrespo@iMac-de-david.local>
2023-07-18 21:22:32 +0200

    Fruits

---
+++ components/Header/Header.js
@@ -0,0 +1,21 @@
+import "./Header.css";
+
+const template = () => {
+    return `
+    <h1>NutriFruits</h1>
+    <nav>
+    <div>
+    <input type="text" id="my-input" placeholder="Choose a fruit..."/>
+    <button id="my-btn">Search</button>
+    </div>
+    <article id="pizarra-data"></article>
+    </nav>
+    `;
+  };
+
+
+  const Header = () => {
+    document.querySelector("header").innerHTML = template();
+  };
+
+  export default Header;
