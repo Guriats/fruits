91c42f3c18bf21786e783bdee4cc7517bd4389de
david crespo <davidcrespo@iMac-de-david.local>
2023-07-26 15:51:11 +0200

    Frutas

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -6,14 +6,16 @@ const template = () => {
     
     <h1>NutriFruits</h1>
     
-      
+     
     <div id="navhome">
     <a href="#null" id="home-link">Home</a>
     <a href="#null" id="about-link">About us</a>
     <a href="#null" id="contact-link">Contact</a>
     <a href="#null" id="api-link">Api</a>
+   <div id=bot>
     <input type="text" id="my-input" placeholder="Choose a fruit..."/>
     <button id="my-btn">Search</button>
+ </div>
     </div>
   
 
